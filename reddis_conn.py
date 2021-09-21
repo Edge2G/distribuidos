93dae72b111db0f147992f86f6a2c7f3bdfe71d7
Saleroo <50508769+Saleroo@users.noreply.github.com>
2021-09-20 23:43:36 -0300

    conexión a redis por python

---
+++ reddis_conn.py
@@ -0,0 +1,13 @@
+import redis
+r = redis.Redis(host="localhost", port=6379, db=0)
+
+r.set('hello','world') #llave=hello valor=world
+value = r.get('hello')
+print(value)
+
+# Candos para la terminal:
+# config get maxmemory-policy : te permite saber la politica de tu cache.
+# config set maxmemory-policy volatile-lru : cambia las politicas del cache a lru.
+# config get maxmemory : obtienes el maximo de la memoria cache -----> si sale 0 es xq tienen memoria infinita.
+# config set maxmemory 20M: modifica el maximo de la memoria cache 20MB.
+
