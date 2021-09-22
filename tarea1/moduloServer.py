2cb61aa9b3e7056681ce7d30d13be05c25db83a2
Saleroo <50508769+Saleroo@users.noreply.github.com>
2021-09-22 19:21:04 -0300

    implementacion redis

--- tarea1/moduloServer.py
+++ tarea1/moduloServer.py
@@ -5,6 +5,9 @@ import protoBusqueda_pb2_grpc as pb2_grpc
 import protoBusqueda_pb2 as pb2
 import json
 import re 
+import redis
+import os
+import pickle
 
 
 
@@ -14,6 +17,10 @@ class servicioBusqueda(pb2_grpc.ServicioBusquedaServicer):
         pass
 
     def ObtenerRespuesta(self, request, context):
+
+        #inicializadon y configurando redis
+        r = redis.Redis(host="localhost", port=6379, db=0)
+
         string_de_busqueda = request.nombre
         result = f'Servidor buscando el item: "{string_de_busqueda}"'
 
@@ -21,19 +28,36 @@ class servicioBusqueda(pb2_grpc.ServicioBusquedaServicer):
         
         nombre=string_de_busqueda
 
-        with open('inventario.json') as file:
-            data = json.load(file)
+        #verificar si esta en cache primero
+        if r.get(nombre) != None:
 
-            for producto in data['productos']:
+            print("--------Estaba en cache------------")
 
-                if re.search(nombre, producto['nombre']):
-                    result = {'id': producto['id'], 'nombre': producto['nombre'] , 'marca': producto['marca'], 'precio':producto['precio'] }
-                    Listresult.append(result)
+            #obtener  por cache 
+            read_dict = r.get(nombre)
+            search_res = pickle.loads(read_dict) #leer los bits
 
-        search_res = {'item': Listresult}
-        print(type(search_res))
+            return pb2.ListaResultante(**search_res)
 
-        return pb2.ListaResultante(**search_res)
+        #no estaba en cache
+        else:
+            print("--------NO Estaba en cache------------")
+            with open('inventario.json') as file:
+                data = json.load(file)
+
+                for producto in data['productos']:
+
+                    if re.search(nombre, producto['nombre']):
+                        result = {'id': producto['id'], 'nombre': producto['nombre'] , 'marca': producto['marca'], 'precio':producto['precio'] }
+                        Listresult.append(result)
+
+            search_res = {'item': Listresult}
+
+            #ingresar a cache
+            p_search_res = pickle.dumps(search_res) #pasar a bits para luego guardar como string el diccionario.
+            r.set(nombre,p_search_res)
+
+            return pb2.ListaResultante(**search_res)
 
 
 def serve():
